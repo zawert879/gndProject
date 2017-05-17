a7b7242f5d0cb87449b5550edc138a59d8cb8e5b
IGorRozhkov <zawert23879@gmail.com>
2017-05-17 13:28:19 +0300

    i1

---
+++ app/src/main/java/com/example/a1/gndproject/Splash.java
@@ -0,0 +1,33 @@
+package com.example.a1.gndproject;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Thread myThread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    sleep(3000);
+
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+//     super.run();
+            }
+
+        };
+        myThread.start();
+
+    }
+}
