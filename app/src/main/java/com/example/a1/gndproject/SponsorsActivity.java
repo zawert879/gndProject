59a5e6786091209db4d70b25d1f13fc8944d3efb
IGorRozhkov <zawert23879@gmail.com>
2017-05-18 12:32:28 +0300

    qq

--- app/src/main/java/com/example/a1/gndproject/SponsorsActivity.java
+++ app/src/main/java/com/example/a1/gndproject/SponsorsActivity.java
@@ -1,20 +1,36 @@
 package com.example.a1.gndproject;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AppCompatActivity;
 import android.webkit.WebView;
 
-public class SponsorsActivity extends AppCompatActivity {
-
+public class SponsorsActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener {
+    private SwipeRefreshLayout mSwipeRefreshLayout;
     private String URL = "https://m.vk.com/";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sponsors);
 
+        mSwipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.refresh);
+        mSwipeRefreshLayout.setOnRefreshListener(SponsorsActivity.this);
+
         WebView web = (WebView)findViewById(R.id.webSponsors);
         web.getSettings().setJavaScriptEnabled(true);
         web.loadUrl(URL);
 
+    }
+
+    @Override
+    public void onRefresh() {
+        mSwipeRefreshLayout.setRefreshing(true);
+        mSwipeRefreshLayout.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mSwipeRefreshLayout.setRefreshing(false);
+
+            }
+        },3000);
     }
 }
